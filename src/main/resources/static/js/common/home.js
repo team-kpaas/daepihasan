c33e613e298d1be1f662493898cd12b222c5b9bb
jazz5684 <rdw77477410@gmail.com>
2025-08-22 10:53:52 +0900

    feat: 홈 화면 디자인 카드 추가 카드 에니메이션적용

---
+++ src/main/resources/static/js/common/home.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+
+    slidesPerView: 3,
+    loop:true,
+    initialSlide:0,
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+    observer: true,
+    observeParents: true,
+});
+
